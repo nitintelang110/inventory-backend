7b97b0f8f3112a44d9412def9081d2be36839f30
nitin telang <nitintelang110@gmail.com>
2024-05-27 12:10:16 +0530

    commit

--- utils/db.js
+++ utils/db.js
@@ -4,9 +4,9 @@ import mysql from 'mysql2';
 const db = mysql.createConnection({
     
     host:"localhost",
-    user:"root",
-    password:"usbw",
-    database:"inventory"
+    user:"ntcoder1_inventory_app",
+    password:"HoB%9nOMPHUA",
+    database:"ntcoder1_inventory"
     
 })
 
